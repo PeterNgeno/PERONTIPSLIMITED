f0b8719cb4391f467829baef375e03d98a031018
PeterNgeno <peterkipsangngeno@gmail.com>
2024-11-28 21:21:28 +0300

    Update quiz.js

--- models/quiz.js
+++ models/quiz.js
@@ -1,17 +1,19 @@
 // models/Quiz.js
-const { DataTypes } = require('sequelize');
-const sequelize = require('../db'); // Sequelize instance
 
-const Quiz = sequelize.define('Quiz', {
-  section: {
-    type: DataTypes.STRING,
-    allowNull: false,
-    unique: true, // Ensure that each section (A, B, etc.) is unique
+const mongoose = require('mongoose');
+
+const quizSchema = new mongoose.Schema({
+  question: {
+    type: String,
+    required: true,
   },
-  questions: {
-    type: DataTypes.JSONB, // Store questions and answers as JSON
-    allowNull: false,
+  options: [String],
+  answer: {
+    type: String,
+    required: true,
   }
 });
+
+const Quiz = mongoose.model('Quiz', quizSchema);
 
 module.exports = Quiz;
