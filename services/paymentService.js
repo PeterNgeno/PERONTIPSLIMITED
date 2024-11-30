d1eaa1508a1ec85560f01fb42fcd16cea615b261
PeterNgeno <peterkipsangngeno@gmail.com>
2024-11-30 22:26:42 +0300

    Update paymentService.js

--- services/paymentService.js
+++ services/paymentService.js
@@ -1,15 +1,26 @@
-const Payment = require('../models/Payment');
+const db = require('../db');  // Assuming db.js contains SQLite connection
 
 // Service to create a payment record
 function createPaymentService(userId, amount, paymentMethod, status) {
-  const newPayment = new Payment({
-    userId,
-    amount,
-    paymentMethod,
-    status,
+  return new Promise((resolve, reject) => {
+    const query = `
+      INSERT INTO payments (userId, amount, paymentMethod, status)
+      VALUES (?, ?, ?, ?)
+    `;
+    db.run(query, [userId, amount, paymentMethod, status], function(err) {
+      if (err) {
+        reject(err);  // Reject with the error if something goes wrong
+      } else {
+        resolve({
+          id: this.lastID,  // The ID of the newly created payment record
+          userId,
+          amount,
+          paymentMethod,
+          status
+        });
+      }
+    });
   });
-
-  return newPayment.save();
 }
 
 module.exports = {
